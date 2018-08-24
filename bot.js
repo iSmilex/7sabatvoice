9cdab050d9babc4a45bcc10cbdd39cc98c23961a
iSmilex <38261980+iSmilex@users.noreply.github.com>
2018-08-24 06:18:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,25 @@
 const Discord = require('discord.js');
 const first = new Discord.Client();
 
-first.login("NDgyMzg1ODc4MjkxODQxMDQ1.DmEVWQ.WO8eETMNBRcrYxxlNHzFDiKH-Is")
+first.channels.get("482239001173164042").join();
+
+client.on('guildMemberAdd',async member => {
+  if(member.guild.id !== '468167578855014411') return;
+
+  member.guild.channels.get("481942220191170580").send(`** Welcome To __Dynasty__ Server , Have a precious time**. :tada:`);
+});
+
+first.login("NDgyMzg1ODc4MjkxODQxMDQ1.DmEVWQ.WO8eETMNBRcrYxxlNHzFDiKH-Is");
+
+const second = new Discord.Client();
+
+client.on('guildMemberAdd',async member => {
+  if(member.guild.id !== '468167578855014411') return;
+
+  member.guild.channels.get("481942220191170580").send(`** Welcome To __Dynasty__ Server , Have a precious time**. :tada:`);
+});
+
+
+second.channels.get("482238987344543755").join();
+
+second.login("NDgyNDAwNjA5ODI0NDA3NjEw.DmEXQA.q6XMDxgz7-uVdStJaiuMOChuwNA");
