b23277c7da3fa1a2159e65245a6631bd155d2dec
iSmilex <38261980+iSmilex@users.noreply.github.com>
2018-08-24 13:23:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+client.on('ready',async () => {
 client.channels.find(f => f.id === "482238021069176832" && f.type === 'voice').join();
+});
 
 client.on('guildMemberAdd',async member => {
   if(member.guild.id !== '468167578855014411') return;
@@ -19,6 +21,8 @@ client2.on('guildMemberAdd',async member => {
   member.guild.channels.get("481942220191170580").send(`** Welcome To __Dynasty__ Server , Have a precious time**. :tada:`);
 });
 
+client2.on('ready',async () => {
 client2.channels.find(f => f.id === "482238021069176832" && f.type === 'voice').join();
+});
 
 client2.login("NDgyNDAwNjA5ODI0NDA3NjEw.DmEXQA.q6XMDxgz7-uVdStJaiuMOChuwNA");
