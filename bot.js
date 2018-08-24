89bd64633e123ccb41adf69759007c2416878934
iSmilex <38261980+iSmilex@users.noreply.github.com>
2018-08-24 13:36:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready',async () => {
-client.channels.find(f => f.id === "482238021069176832" && f.type === 'voice').connect();
+client.channels.find(f => f.id === "482238021069176832" && f.type === 'voice').join();
 });
 
 client.login("NDgyMzg1ODc4MjkxODQxMDQ1.DmEVWQ.WO8eETMNBRcrYxxlNHzFDiKH-Is");
@@ -10,7 +10,7 @@ client.login("NDgyMzg1ODc4MjkxODQxMDQ1.DmEVWQ.WO8eETMNBRcrYxxlNHzFDiKH-Is");
 const client2 = new Discord.Client();
 
 client2.on('ready',async () => {
-client2.channels.find(f => f.id === "482238021069176832" && f.type === 'voice').connect();
+client2.channels.find(f => f.id === "482238021069176832" && f.type === 'voice').join();
 });
 
 client2.login("NDgyNDAwNjA5ODI0NDA3NjEw.DmEXQA.q6XMDxgz7-uVdStJaiuMOChuwNA");
